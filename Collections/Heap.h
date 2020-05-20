b3e24607df129791dfda4f03ade5b5d0352a51dd
MOHAND99 <muhanned122000@yahoo.com>
2020-05-20 17:52:28 +0200

    errors commit

--- Collections/Heap.h
+++ Collections/Heap.h
@@ -1,23 +1,100 @@
 
 #ifndef SIMPLE_INTERPRETER_HEAP_H
 #define SIMPLE_INTERPRETER_HEAP_H
+#include <string>
+#include <bits/stdc++.h>
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+
+
+
 template <class k, class v>
-struct Node{
+class Node{
+private:
     k key;
     v value;
-    Node *left;
-    Node *right;
+public:
+    Node(k key, v value) {
+        this->key = key;
+        this->value = value;
+    }
+
+    k getKey() const {
+        return key;
+    }
+
+    v getValue() const {
+        return value;
+    }
 };
 
 template <class k, class v>
 class Heap {
 private:
-    int count;
+    //Node<k, v> *arr;
+    Node<k, v> *arr;
+    //vector<Node<k, v>> vector;
+    int n; // number of added nodes
 public:
- void put(k key,v value);
- Node<k, v> extractMin();
- void sort();
+    Heap(int size) {
+       // arr = (Node<k, v>*) malloc(sizeof(Node<k, v>) * size);
+       arr = new Node<k, v>[size];
+        n = 0;
+    }
+    void put(k key,v value) {
+        int i;
+        for(i=n; arr[(i-1)/2]. > value && i>0; i=(i-1)/2 ) {
+            arr[i] = arr[(i - 1) / 2];
+            //((Node<k, v>)arr[i]).value = ((Node<k, v>)arr[(i-1)/2]).value;
+            cout << "value "<< ((Node<k, v>) arr[i]).v << "key " << ((Node<k, v>)arr[i]).key << endl;//debug
+        }
+        arr[i].value = value;
+        arr[i].key = key;
+        n++;
+    }
+    void insertMap(map<k, v> map);
+    Node<k, v> extractMin();
+    void sort();
+    ~Heap();
 };
+
+
+
+
+template<class k, class v>
+Node<k, v> Heap<k, v>::extractMin() {
+    return Node<k, v>();
+}
+
+template<class k, class v>
+void Heap<k, v>::sort() {
+
+}
+
+template<class k, class v>
+Heap<k, v>::~Heap() {
+   // delete[] arr;
+}
+
+template<class k, class v>
+void Heap<k, v>::insertMap(map<k, v> map) {
+    arr = realloc(arr, map.size() * sizeof(Node<k, v>));
+    for(pair<k, v> element : map)
+        put(element.first, element.second);
+}
+
+/*template<class k, class v>
+Heap<k, v>::Heap(int size) {
+   // arr = (Node<k, v>*) malloc(sizeof(Node<k, v>) * size);
+    arr =(Node<k, v>*) malloc(sizeof(Node<k, v>) * size);
+    n = 0;
+    cout <<7;//debug
+}*/
 
 
 #endif //SIMPLE_INTERPRETER_HEAP_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include "Evaluation/ExpressionEvaluator.h"
+#include "Collections/Heap.h"
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -17,23 +19,30 @@ int main() {
 //        // TODO: Add the variable to BST and heap.
 //
 //    }
-    while (true) {
-        cout << "> ";
-        string line;
-        getline(cin, line);
-        unordered_map<string, double> map;
-        ExpressionEvaluator eval(line, map);
-        try {
-            double result = eval.evaluate();
-            cout << "The result is: " << result << endl << endl;
-        } catch (const char *e) {
-            cout << e << endl;
-        } catch (string e) {
-            cout << e << endl;
-        }
+//    while (true) {
+//        cout << "> ";
+//        string line;
+//        getline(cin, line);
+//        unordered_map<string, double> map;
+//        ExpressionEvaluator eval(line, map);
+//        try {
+//            double result = eval.evaluate();
+//            cout << "The result is: " << result << endl << endl;
+//        } catch (const char *e) {
+//            cout << e << endl;
+//        } catch (string e) {
+//            cout << e << endl;
+//        }
 
 
-    }
+//    }
 
+ Heap<string, double> heap(3);
+// cout << "string size = " << sizeof(string) << endl;
+//    cout << "double size = " << sizeof(double) << endl;
+
+ heap.put("aaa", 15);
+ heap.put("bb", 3);
+ heap.put("wee", 4.6);
     return 0;
 }
