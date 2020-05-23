6cf7664f80e91d06637d6f8b69ce6b06ab427c7a
MOHAND99 <muhanned122000@yahoo.com>
2020-05-23 05:45:21 +0200

    small edit

--- Statements/IfStatement.cpp
+++ Statements/IfStatement.cpp
@@ -8,7 +8,6 @@ bool IfStatement::isValid(string statement) {
     int ifCount, colonCount;// counters for ifs and colons, they must be = 1 for valid if statement
     ifCount = colonCount = 0;
     if (statement.find("if") != std::string::npos)  ifCount++;
-    if (statement.find(":") != std::string::npos)   colonCount++;
-    if(ifCount == 1 && colonCount == 1) return true;
-    return false;
+    if (statement.find(':') != std::string::npos)   colonCount++;
+    return ifCount == 1 && colonCount == 1;
 }
