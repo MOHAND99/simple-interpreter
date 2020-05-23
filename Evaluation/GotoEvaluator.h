03c328320800f4949b08322949a4344c8af4d0b0
mohamed zayton <mzayton10@gmail.com>
2020-05-23 22:50:29 +0200

    GOTO_STATEMENT

--- Evaluation/GotoEvaluator.h
+++ Evaluation/GotoEvaluator.h
@@ -6,12 +6,11 @@
 #define SIMPLE_INTERPRETER_GOTOEVALUATOR_H
 #include <map>
 #include <string>
-#include <wsman.h>
-
+using namespace std;
 class GotoEvaluator {
 private:
-    map<string,string> *fileLines;
-    unordedmap<string
+
+
 };
 
 
