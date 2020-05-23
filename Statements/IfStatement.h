012a1749b4625ccfba6a0bbf022fa3f9cf9711d8
MOHAND99 <muhanned122000@yahoo.com>
2020-05-23 06:41:59 +0200

    execute function

--- Statements/IfStatement.h
+++ Statements/IfStatement.h
@@ -1,19 +1,20 @@
-//
-// Created by Mhnd Bshar on 5/23/2020.
-//
-
 #ifndef SIMPLE_INTERPRETER_IFSTATEMENT_H
 #define SIMPLE_INTERPRETER_IFSTATEMENT_H
 
 
 #include "Statement.h"
+#include <string>
+#include <iostream>
 
 class IfStatement : public Statement {
 private:
     string statement;
     unordered_map<string, double> variables;
+    string conditionExpression;
+    Statement conditioned_Statement;
 public:
     static bool isValid(const string& statement);
+    void execute() override;
 };
 
 
