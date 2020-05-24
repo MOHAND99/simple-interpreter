29fa7c6f1853fdbba4ebe82de10cd89be4637912
mohamed zayton <mzayton10@gmail.com>
2020-05-24 16:33:40 +0200

    Parser_With_Label

--- Statements/GotoStatement.cpp
+++ Statements/GotoStatement.cpp
@@ -6,17 +6,18 @@
 
 bool GotoStatement::isValid(string statement) {
     char stat[statement.length()];
+    string keyWord= "goto";
     string label = "";
     char pointerStat[statement.length()];
     std::copy(statement.begin(), statement.end(), stat);
     int startWithNumber = 0;
     int gotoIndex = -1;
-    gotoIndex = statement.find_first_of("goto");
+    gotoIndex = statement.find_first_of(keyWord);
     ///if statement contain goto
     if (gotoIndex != -1) {
-        if (stat[gotoIndex + 4] != ' ')
+        if (stat[gotoIndex + keyWord.length()] != ' ')
             return false;
-        label = statement.substr(gotoIndex + 4, statement.length() - gotoIndex - 4);
+        label = statement.substr(gotoIndex + keyWord.length(), statement.length() - gotoIndex - keyWord.length());
         std::copy(label.begin(), label.end(), pointerStat);
         pointerStat[label.length()] = '\0';
         ///Check the label variable
@@ -38,7 +39,7 @@ bool GotoStatement::isValid(string statement) {
                 return false;
             }
         }
-        for (int k = gotoIndex; k >=0 ; k--) {
+        for (int k = gotoIndex-1; k >=0 ; k--) {
             if (stat[k]!=' ')
                 return false;
         }
